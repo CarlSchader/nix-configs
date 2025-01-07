ae4ab2d866076d99c69e5a4428bd623e06852add
Carl Schader <carlschader@gmail.com>
2025-01-06 21:50:13 -0500

    nixos hostname

--- x86/flake.nix
+++ x86/flake.nix
@@ -2,12 +2,10 @@
   description = "Flake for x86 systems";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = all@{ self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }@inputs:
   {
     # Used with `nixos-rebuild --flake .#<hostname>`
     # nixosConfigurations."<hostname>".config.system.build.toplevel must be a derivation
