e754419d816bf21725039f8da3eb923b89166bd6
Carl Schader <carl.schader@saronic.com>
2025-01-08 10:48:34 -0600

    human readable ll

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -56,7 +56,75 @@
             '';
             shellAliases = {
               t = "tmux";
-              ll = "ls -lG";
+              ll = "ls -lhG";
+              ls = "ls -G";
+              l = "ls -G";
+            };
+          };
+
+          home.stateVersion = "24.11";
+        };
+      };
+
+      # User configuration
+      users.users.carlschader = {
+        name = "carlschader";
+        home = "/Users/carlschader";
+      };
+    };
+    configuration-work = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = [ 
+        pkgs.vim
+        pkgs.tailscale
+      ];
+
+      services.tailscale = {
+        enable = true;
+        # interfaceName = "userspace-networking";
+      };
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # Add home-manager configuration here
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.carlschader = { pkgs, ... }: {
+          home.packages = [ 
+            pkgs.neovim 
+            pkgs.nixd
+            pkgs.tmux
+            pkgs.git
+            pkgs.gh
+            pkgs.cargo
+          ];
+
+          programs.zsh = {
+            enable = true;
+            enableCompletion = true;
+            initExtra = ''
+              autoload -U colors && colors
+              PS1="%{$fg[green]%}%n%{$reset_color%}@%{$fg[green]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
+            '';
+            shellAliases = {
+              t = "tmux";
+              ll = "ls -lhG";
               ls = "ls -G";
               l = "ls -G";
             };
@@ -85,7 +153,7 @@
 
     darwinConfigurations."Carls-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       modules = [ 
-        configuration
+        configuration-work
         home-manager.darwinModules.home-manager
       ];
     };
