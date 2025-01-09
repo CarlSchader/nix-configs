3eba01e700c30654649fbe467feabf7c0ff4adeb
Carl Schader <carl.schader@saronic.com>
2025-01-09 13:06:54 -0600

    add self

--- darwin.nix
+++ darwin.nix
@@ -1,6 +1,6 @@
 # nix-darwin configuration
 
-{ pkgs, ... }: {
+{ self, pkgs, ... }: {
     # List packages installed in system profile. To search by name, run:
     # $ nix-env -qaP | grep wget
     environment.systemPackages =
